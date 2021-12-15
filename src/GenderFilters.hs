3445be66f9f76769487e779aa33c3756835d2995
SakshiChahande <sakchahande@gmail.com>
2021-12-15 21:08:23 +0000

    just removed extra prints

--- src/GenderFilters.hs
+++ src/GenderFilters.hs
@@ -25,26 +25,22 @@ instance FromRow StudentScoreGender where
 
 getGender :: Connection -> IO [StudentScoreGender]
 getGender conn = do
-    putStr "GetGender \n"
     let roll_no = 1 :: Int 
     let sql_query = "Select studentData.roll_no,math,reading,writing,gender FROM studentData INNER JOIN studentScores ON studentData.roll_no = studentScores.roll_no AND 1=?"
     query conn sql_query [roll_no]
 
 
 getStudentDataGender conn level = do
-    putStr "GetStudentDataGender \n"
     let sql_query = "Select studentData.roll_no,math,reading,writing,gender FROM studentData INNER JOIN studentScores ON studentData.roll_no = studentScores.roll_no AND studentData.gender = ?"
     query conn sql_query [level]
 
 showGenderAnalysis :: Connection -> IO ()
 showGenderAnalysis conn = do
-    putStr "Inside GenderAnalysis \n"
     studentData <- getGender conn
     close conn
     let allTypes = map gender_marks studentData
     let distinctGenType = uniq allTypes
     mapM_ (analyseEachType conn) distinctGenType
-    print "HelloGen \n"
 
 
 -- loopthough :: [String] -> [[String]]
@@ -52,7 +48,6 @@ loopthough [] = []
 loopthough (x:xs) = (analyseEachType x) : loopthough xs 
 
 analyseEachType conn level= do
-    putStr "Inside analysis \n"
     conn <- open "covid.sqlite"
     response <- getStudentDataGender conn level
 
